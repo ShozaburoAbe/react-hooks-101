12db8bd8edd1a115220bd5c6f581b0499a3fc84c
ShozaburoAbe <61729075+ShozaburoAbe@users.noreply.github.com>
2020-09-12 02:30:44 +0900

    create operation log reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,9 @@
 import {combineReducers} from 'redux'
-import events from './events'
 
-export default combineReducers({events})
+import events from './events'
+import operationLogs from './operationLogs'
+
+export default combineReducers({
+  events,
+  operationLogs
+})
